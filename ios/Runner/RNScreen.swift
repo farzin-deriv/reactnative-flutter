a03020436689d80480b8cf5104030ebbb2df93e9
Farzin Mirzaie <farzin@firstsource.tech>
2021-06-21 09:57:00 +0430

    [WIP] Merge FlutterApp and RNApp (iOS)

---
+++ ios/Runner/RNScreen.swift
@@ -0,0 +1,8 @@
+//
+//  RNScreen.swift
+//  Runner
+//
+//  Created by Farzin on 6/21/21.
+//
+
+import Foundation
---
+++ ios/Runner/RNView.swift
@@ -0,0 +1,20 @@
+//
+//  RNView.swift
+//  Runner
+//
+//  Created by Farzin on 6/21/21.
+//
+
+import SwiftUI
+
+struct RNView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RNView_Previews: PreviewProvider {
+    static var previews: some View {
+        RNView()
+    }
+}
